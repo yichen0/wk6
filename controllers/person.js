af03c496869fac5a1a90fde22bc369067c74e7f4
yichen0 <pp362886@yahoo.com.tw>
2017-04-12 20:50:27 +0800

    new

--- controllers/person.js
+++ controllers/person.js
@@ -1,7 +1,7 @@
 var Person = require('../models/person');
 
 module.exports = (req, res, next)=>{
-	// res.send('Thank you!');
+	 res.send('Thank you!');
 	var person = new Person({
 		firstname: req.body.firstname,
 		lastname: req.body.lastname
